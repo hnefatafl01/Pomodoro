c0689d313570ae323c5c2b4a7b42a9e8e7bfae5f
Kevin Erickson <kevin.erick01@gmail.com>
2017-07-12 21:39:53 -0600

    ticking timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  private setTimeoutHandler: any;
+  private setIntervalHandler: any;
+  title = 'Super Pom';
+  time = 0;
+  timer(ticker: any) {
+    this.setTimeoutHandler = setTimeout(() => { }, 6000);
+  }
+
+  tick() {
+    this.setIntervalHandler = setInterval(() => {
+      this.time += 1;
+      console.log(this.time);
+    }, 1000);
+  }
+
+  start(){
+    this.timer(this.tick());
+    console.log("start this shiz");
+  }
+  stop() {
+    clearTimeout(this.setTimeoutHandler);
+    clearInterval(this.setIntervalHandler);
+    console.log("stop this shiz");
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { MdButtonModule,MdToolbarModule,MdCardModule,MdGridListModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdToolbarModule,
+    MdButtonModule,
+    MdCardModule,
+    MdGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
